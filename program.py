54e0c27c694f016452725ad56369b3b0c34208e2
Raymond Malicdem <mobdawg13@yahoo.com>
2021-09-07 14:04:36 +0400

    added hash bang, module comments and checks if it is to be run as main

--- program.py
+++ program.py
@@ -1,1 +1,12 @@
-print("hello")
+#!/usr/bin/env python3
+
+# Author: Raymond Malicdem
+# Date Created: 4-Sep-2021 
+# Description: Terminal based python script program that prints text
+#				when run either as a program or imported a as module
+
+
+if __name__ == "__main__":
+	print("hello world as main module")
+else:
+	print("hello world from imported module")
